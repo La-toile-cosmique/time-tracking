5b3e2123a6fffa6734bd4cf3d8130c18b84d14b3
Creasmart <contact@creasmart.fr>
2014-05-16 18:59:02 +0200

    A lot of change: New design from scratch

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,34 @@
 var gulp = require('gulp'),
-	less = require('gulp-less');
-	plumber	 = require('gulp-plumber');
-	gulp_src = './public/css/*.less';
-
+	less = require('gulp-less'),
+	plumber	 	= require('gulp-plumber'),
+	plumber	 	= require('gulp-plumber'),
+	browserSync = require('browser-sync'),
+	gulp_src 	= './public/css/main.less';
 
 gulp.task('less-compile', function() {
 	gulp.src(gulp_src)
 	  .pipe(plumber())
 	  .pipe(less())
-	  .pipe(gulp.dest('./public/css/'));
+	  .pipe(gulp.dest('./public/css/'))
+	  .pipe(browserSync.reload({stream:true}));
 });
 
+gulp.task('browser-sync', function() {
+    browserSync.init( 
+    	null, 
+    	{proxy: "localhost:4000"}
+	);
+});
 
-gulp.task('default', function() {
+gulp.task('browser-syncouille', function(){
 
-	gulp.watch(gulp_src, ['less-compile']);
+	browserSync.reload();
+
+});
+
+gulp.task('default', ['browser-sync'], function() {
+
+	gulp.watch([gulp_src], ['less-compile']);
+	gulp.watch(['./public/**/*.html', './public/*.html', './public/**/*.js'], ['browser-syncouille']);
 
 });
