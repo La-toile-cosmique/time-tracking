0e030786784412eae9f0ab15f82b7dff1abef109
Creasmart <contact@creasmart.fr>
2014-05-17 11:01:25 +0200

    Css modifications

--- data.js
+++ data.js
@@ -4,9 +4,20 @@ mongoose.connect( 'mongodb://localhost/test' );
 
 var Schema = mongoose.Schema;
 
-var Project = new Schema({
+var Projects = new Schema({
 	name         	: { type: String, default: "New project" },
 	description		: { type: String, default: "Description of the new project" },
+	estimed_time 	: { type: Number, default: 1 },
+	seconds			: { type: Number, default: 0 },
+	active			: { type: Boolean, default: false },
+	start_date 		: { type: Date, default: Date() },
+	steps  			: [Steps],
+	buff 			: Buffer
+});
+
+var Steps = new Schema({
+	name         	: { type: String, default: "New step" },
+	description		: { type: String, default: "Description of the step" },
 	estimed_time 	: { type: Number, default: 1 },
 	seconds			: { type: Number, default: 0 },
 	active			: { type: Boolean, default: false },
@@ -14,7 +25,7 @@ var Project = new Schema({
 	buff 			: Buffer
 });
 
-module.exports = mongoose.model( 'projects', Project );
+module.exports =  mongoose.model( 'projects', Projects );
 
 
 
